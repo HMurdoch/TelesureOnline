3bd9d8e7fbea829a093844d4554c1c058a2cdbc2
HughMurdoch <hughmurdoch@gmail.com>
2022-05-24 23:12:38 +0200

    Added POST calls to REST API for all parts of speech

--- NodejsWebApi/api/words-by-name.js
+++ NodejsWebApi/api/words-by-name.js
@@ -33,7 +33,6 @@ router.get('/nouns/:prefix', async (req, res) => {
         res.status(500).json(error);
     }
 });
-
 router.post('/nouns/:word', async (req, res) => {
     try {
         pow = "n.";
@@ -63,11 +62,39 @@ router.get('/verbs/:prefix', async (req, res) => {
         res.status(500).json(error);
     }
 });
+router.post('/verbs/:word', async (req, res) => {
+    try {
+        pow = "v.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 router.get('/adjectives/:prefix', async (req, res) => {
     try {
         pow = "a.";
         let query = `SELECT DISTINCT TOP 20 word FROM dbo.entries WHERE wordtype LIKE ('%${pow}%') AND word LIKE('${req.params.prefix}%');`;
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.post('/adjectives/:word', async (req, res) => {
+    try {
+        pow = "a.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
         await pool.connect();
         const result = await pool.request().query(query);
         const words = result.recordset;
@@ -91,11 +118,39 @@ router.get('/adverbs/:prefix', async (req, res) => {
         res.status(500).json(error);
     }
 });
+router.post('/adverbs/:word', async (req, res) => {
+    try {
+        pow = "adv.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 router.get('/pronouns/:prefix', async (req, res) => {
     try {
         pow = "pron.";
         let query = `SELECT DISTINCT TOP 20 word FROM dbo.entries WHERE wordtype LIKE ('%${pow}%') AND word LIKE('${req.params.prefix}%');`;
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.post('/pronouns/:word', async (req, res) => {
+    try {
+        pow = "pron.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
         await pool.connect();
         const result = await pool.request().query(query);
         const words = result.recordset;
@@ -119,6 +174,20 @@ router.get('/prepositions/:prefix', async (req, res) => {
         res.status(500).json(error);
     }
 });
+router.post('/prepositions/:word', async (req, res) => {
+    try {
+        pow = "prep.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 router.get('/conjunctions/:prefix', async (req, res) => {
     try {
@@ -133,5 +202,45 @@ router.get('/conjunctions/:prefix', async (req, res) => {
         res.status(500).json(error);
     }
 });
+router.post('/conjunctions/:word', async (req, res) => {
+    try {
+        pow = "conj.";
+        let query = `INSERT INTO dbo.entries (word, wordtype, definition) VALUES ('${req.params.word}', '${pow}', 'Custom word');`;
+        console.log(query);
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
 
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.get('/sentences', async (req, res) => {
+    try {
+        let query = `SELECT sentences FROM dbo.sentences;`;
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const sentences = result.recordset;
+
+        res.status(200).json(sentences);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+router.post('/sentences/:sentence', async (req, res) => {
+    try {
+        pow = "conj.";
+        let query = `INSERT INTO dbo.sentences (sentences) VALUES ('${req.params.sentence}');`;
+        console.log(query);
+        await pool.connect();
+        const result = await pool.request().query(query);
+        const words = result.recordset;
+
+        res.status(200).json(words);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 module.exports = router;
